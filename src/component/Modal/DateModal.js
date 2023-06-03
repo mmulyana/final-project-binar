4c0299aecde5193ab842abb88fdafc5879e9f510
mulyant20 <mulyan.t20@gmail.com>
2023-06-03 19:19:12 +0700

    style: custom style for date picker

--- src/component/Modal/DateModal.js
+++ src/component/Modal/DateModal.js
@@ -1,17 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import withModal from './withModal'
 import { DayPicker } from 'react-day-picker'
-import 'react-day-picker/dist/style.css'
 
 function Dates({ isSingle = false, toggleModal }) {
   const [selected, setSelected] = useState(new Date())
 
-  console.log(selected)
-
   return (
     <div className='relative max-w-[968px] mx-auto h-full'>
       <div className='absolute top-[540px] left-1/2 -translate-x-1/2 z-50 w-full'>
-        <div className='max-w-[635px] h-fit bg-white mx-auto rounded-2xl py-4'>
+        <div className='max-w-[635px] h-fit bg-white mx-auto rounded-2xl pt-4 pb-1'>
           <DayPicker
             selected={selected}
             onSelect={setSelected}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
-import '@/styles/globals.css'
-import '@/styles/custom-date.css'
 import { Poppins } from 'next/font/google'
 
+import '@/styles/globals.css'
+import 'react-day-picker/dist/style.css'
+import '@/styles/custom-date.css'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
