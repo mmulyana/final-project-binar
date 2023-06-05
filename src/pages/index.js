2dd98b1130ac33c664ef34d4416fe440425931ad
Aldi Fahrezi <126686349+Alfarezi1720@users.noreply.github.com>
2023-06-05 09:17:32 +0700

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <></>
-}
+import Navbar from "@/component/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Homepage</h1>
+    </div>
+  );
+};
+
+export default HomePage;
