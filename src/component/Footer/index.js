8296c72dcdae0bdc39a17d84f997b90636c3bd3e
Aldi Fahrezi <126686349+Alfarezi1720@users.noreply.github.com>
2023-06-07 20:44:16 +0700

    footer baru

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -124,7 +124,7 @@ const Footer = () => {
       <MediaQuery maxWidth={786}>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 md:mt-0 ml-auto">
           {/* Card 1 */}
-          <div className="flex flex-col ml-10">
+          <div className="flex flex-col">
             <h4 className="text-lg font-bold mb-4">Tripp</h4>
           </div>
 
