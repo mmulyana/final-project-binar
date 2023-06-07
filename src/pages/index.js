173deeda352d85f654433af2739d0a86a9b367df
salsabilanida <111202012982@mhs.dinus.ac.id>
2023-06-08 01:44:07 +0700

    feat: setup design system homepage section 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import CardFlight from '@/component/Card/CardFlight'
 import DefaultLayout from '@/component/Layout/DefaultLayout'
 import SearchFlight from '@/component/SearchFlight'
+import CardSuggest from '@/component/Card/CardSuggest'
 import { flights } from '@/utils/local'
 import Image from 'next/image'
 import imgBanner from 'public/images/banner.png'
@@ -8,39 +9,48 @@ import imgBanner from 'public/images/banner.png'
 function Home() {
   return (
     <>
-      <div className='w-full h-[610px] relative'>
+      <div className="w-full h-[610px] relative">
         <Image
           src={imgBanner}
-          alt='banner'
+          alt="banner"
           w={1440}
           h={610}
-          className='w-full h-full object-cover object-left-bottom md:object-left-top'
+          className="w-full h-full object-cover object-left-bottom md:object-left-top"
         />
-        <div className='absolute bottom-0 left-0 w-full -mb-80 md:-mb-14 px-4 md:px-0'>
-          <div className='max-w-[1200px] mx-auto'>
+        <div className="absolute bottom-0 left-0 w-full -mb-80 md:-mb-14 px-4 md:px-0">
+          <div className="max-w-[1200px] mx-auto">
             <SearchFlight />
           </div>
         </div>
       </div>
 
       {/* section 2 */}
+      <section className="pt-14 pb-14 mt-20">
+        <div className="container max-w-[1200px] mx-auto">
+          <p className="text-2xl ">Yuk jelajahi dunia kembali</p>
+          <p className="text-sm text-[#A5A4A9]">Nikmati petualangan yang menanti di berbagai destinasi.</p>
+          <div className="mt-10">
+            <CardSuggest />
+          </div>
+        </div>
+      </section>
 
       {/* section 3 */}
-      <section className='bg-white pt-14 pb-20 mt-20'>
-        <div className='container max-w-[1200px] mx-auto'>
-          <h2 className='text-2xl text-[#0E0C25]'>
+      <section className="bg-white pt-14 pb-20 mt-20">
+        <div className="container max-w-[1200px] mx-auto">
+          <h2 className="text-2xl text-[#0E0C25]">
             Destinasi internasional yang banyak diminati
           </h2>
-          <div className='mt-10 grid grid-cols-1 md:grid-cols-4 gap-6'>
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-4 gap-6">
             {flights.map((flight, index) => (
               <CardFlight data={flight} key={index} />
             ))}
           </div>
 
-          <h2 className='text-2xl text-[#0E0C25] mt-14'>
+          <h2 className="text-2xl text-[#0E0C25] mt-14">
             Destinasi Lokal yang banyak diminati
           </h2>
-          <div className='mt-10 grid grid-cols-1 md:grid-cols-4 gap-6'>
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-4 gap-6">
             {flights.map((flight, index) => (
               <CardFlight data={flight} key={index} />
             ))}
@@ -50,7 +60,7 @@ function Home() {
 
       {/* section 4 */}
     </>
-  )
+  );
 }
 
 Home.getLayout = (page) => {
