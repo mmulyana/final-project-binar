e3138858f0a89e9150077f4f470cf03c2bc4409a
Aldi Fahrezi <126686349+Alfarezi1720@users.noreply.github.com>
2023-06-14 20:58:18 +0700

    filter baru

--- src/component/Collapsible/TransitFilterCollapsible.js
+++ src/component/Collapsible/TransitFilterCollapsible.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import withCollapsible from './withCollapsible'
-import { SelectWithCheckbox } from '../Select'
+import React from "react";
+import withCollapsible from "./withCollapsible";
+import { SelectWithCheckbox } from "../Select";
 
 function TransitFilter() {
   return (
-    <div>
-      <p>Berangkat</p>
-      <SelectWithCheckbox name='09.00 - 12.00'/>
+    <div className="">
+      <SelectWithCheckbox name="Langsung" />
     </div>
-  )
+  );
 }
 
-const TransitFilterCollapsible = withCollapsible(TransitFilter)
-export default TransitFilterCollapsible
+const TransitFilterCollapsible = withCollapsible(TransitFilter);
+export default TransitFilterCollapsible;
--- src/component/Select/SelectWithCheckbox.js
+++ src/component/Select/SelectWithCheckbox.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function SelectWithCheckbox({ name, isChecked }) {
   return (
-    <div className='flex items-center justify-between'>
+    <div className="flex items-center justify-between">
       {name}
-      <div>{isChecked ? 'checked' : 'unchecked'}</div>
+
+      <input
+        type="checkbox"
+        checked={isChecked}
+        className="form-checkbox h-4 w-4 ml-40"
+      />
     </div>
-  )
+  );
 }
