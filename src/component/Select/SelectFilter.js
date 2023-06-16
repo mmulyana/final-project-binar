42c92fc8781037dd3971680e5afa7ec609f5cf2d
mulyan <96921234+mmulyana@users.noreply.github.com>
2023-06-16 23:57:33 +0700

    F mulyana filter ticket (#22)
    * setup result page
    
    * feat: select class
    
    * update
    
    * update

--- src/component/Select/SelectFilter.js
+++ src/component/Select/SelectFilter.js
@@ -1,10 +1,20 @@
+import { changeToRupiah } from '@/utils'
 import React from 'react'
 
-export default function SelectFilter({data}) {
+export default function SelectFilter({ data, handleClick }) {
   return (
-    <div>
-        <p>{data.name}</p>
-        <p>{data.price}</p>
+    <div
+      onClick={() => handleClick(data.id)}
+      className={[
+        'flex justify-between flex-col md:flex-row items-end bg-white rounded p-4 border flex-wrap cursor-pointer',
+        data.isActive ? 'border-[#4642FF]' : 'border-gray-300',
+      ].join(' ')}
+    >
+      <p className='text-sm'>{data.name}</p>
+      <div className='text-right text-xs'>
+        <p className='opacity-50 mb-2'>mulai dari</p>
+        <p className='font-medium'>{changeToRupiah(data.price).slice(0, -3)}</p>
+      </div>
     </div>
   )
 }
