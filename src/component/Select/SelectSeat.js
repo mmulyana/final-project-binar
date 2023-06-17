ad3ea8179e000a29bc43d8296e79e066cb93b183
mulyant20 <mulyan.t20@gmail.com>
2023-06-18 02:30:50 +0700

    feat: select seat position

--- src/component/Select/SelectSeat.js
+++ src/component/Select/SelectSeat.js
@@ -1,22 +1,30 @@
-import React from 'react'
-
 export default function SelectSeat({ data, handleSelect }) {
   return (
-    <div className='p-8'>
+    <div className='flex justify-center'>
       <div className='flex gap-2'>
         {data.map((data, index) => (
           <div key={index}>
-            <div className='w-10 h-10 text-center'>
+            <div className='w-8 h-8 text-center'>
               <p>{data.row}</p>
             </div>
             <div className='flex flex-col gap-2'>
               {data.row !== ''
-                ? data.col.map((col, index) => (
-                    <div key={index} className='w-10 h-10 rounded bg-gray-300' />
+                ? data.col.map((col, idx) => (
+                    <div
+                      key={idx}
+                      className={[
+                        'w-8 h-8 rounded cursor-pointer',
+                        col.isSelected ? 'bg-blue-700' : 'bg-gray-300',
+                      ].join(' ')}
+                      onClick={() => handleSelect(data, col, idx)}
+                    />
                   ))
-                : data.col.map((col, index) => (
-                    <p key={index} className='w-10 h-10 flex items-center justify-center'>
-                      {col+1}
+                : data.col.map((col, idx) => (
+                    <p
+                      key={idx}
+                      className='w-8 h-8 flex items-center justify-center'
+                    >
+                      {col + 1}
                     </p>
                   ))}
             </div>
--- src/pages/checkout/order.js
+++ src/pages/checkout/order.js
@@ -3,13 +3,20 @@ import TextfieldPhone from '@/component/Form/TextfieldPhone'
 import { CheckoutLayout } from '@/component/Layout'
 import SelectSeat from '@/component/Select/SelectSeat'
 import { seatesFlight } from '@/utils/local'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
+import Ic_plane from 'public/icon/plane2.svg'
+import Button from '@/component/Button'
+import { changeToRupiah } from '@/utils'
 
 function Order() {
   const router = useRouter()
   const [query, setQuery] = useState(null)
   const [form, setForm] = useState(null)
+  const [seates, setSeates] = useState(seatesFlight)
+  const [lengthSeat, setLengthSeat] = useState(0)
+  const [seatesRemaining, setSeatesRemaining] = useState(0)
 
   useEffect(() => {
     if (!router.query) return
@@ -30,8 +37,21 @@ function Order() {
     addToForm(kidForm, 'kid', query)
     addToForm(babyForm, 'baby', query)
 
+    addToSeates(['adult', 'kid', 'baby'])
+
     return () => setForm(null)
   }, [query, router.query])
+
+  function addToSeates(param) {
+    let length = 0
+    param.forEach((p, index) => {
+      if ([p] in query) {
+        let tmp = parseInt(query[p], 10)
+        length += tmp
+      }
+    })
+    setLengthSeat(length)
+  }
 
   function addToForm(form, type, query) {
     if (type in query) {
@@ -66,28 +86,50 @@ function Order() {
     })
   }
 
+  function handleSelect(data, col, index) {
+    if (seatesRemaining >= lengthSeat) {
+      if (!col.isSelected) return
+    }
+
+    const newStates = seates.map((prev) => {
+      if (prev.row === data.row) {
+        prev.col[index] = {
+          ...prev.col[index],
+          isSelected: !prev.col[index].isSelected,
+        }
+        if (col.isSelected) {
+          setSeatesRemaining((prev) => prev - 1)
+        } else setSeatesRemaining((prev) => prev + 1)
+        return prev
+      }
+      return prev
+    })
+
+    setSeates(newStates)
+  }
+
   if (form === null) {
     return <p>loading...</p>
   }
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[720px_440px] justify-between items-start mt-4 px-4 lg:px-0'>
-      <form onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-4'>
-          <div>
-            <p className='text-lg'>Detail Pemesanan</p>
+    <div className='pt-8'>
+      <div className='px-4 lg:px-0'>
+        <p className='text-xl text-gray-800'>Detail Pemesanan</p>
+      </div>
 
-            <div className='bg-white rounded border border-[#DFDEE2] mt-2 p-4 pb-8'>
-              <p>Data Diri Pemesan</p>
-              <label className='text-sm mt-4 mb-2 block'>Nama</label>
-              <Textfield value='Rengoku kyojuro' disabled />
+      <div className='mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr] gap-4 justify-between items-start px-4 lg:px-0'>
+        <form onSubmit={handleSubmit}>
+          <div className='bg-white rounded border border-[#DFDEE2] pt-6 p-8 pb-10 shadow-sm shadow-gray-300'>
+            <p>Data Diri Pemesan</p>
+            <label className='text-sm mt-4 mb-2 block'>Nama</label>
+            <Textfield value='Rengoku kyojuro' disabled />
 
-              <label className='text-sm mt-4 mb-2 block'>Nomor Telepon</label>
-              <TextfieldPhone value='0872355677' disabled />
+            <label className='text-sm mt-4 mb-2 block'>Nomor Telepon</label>
+            <TextfieldPhone value='0872355677' disabled />
 
-              <label className='text-sm mt-4 mb-2 block'>Email</label>
-              <Textfield value='example@mail.com' disabled />
-            </div>
+            <label className='text-sm mt-4 mb-2 block'>Email</label>
+            <Textfield value='example@mail.com' disabled />
           </div>
 
           <div className='mt-8'>
@@ -309,22 +351,89 @@ function Order() {
             </div>
           </div>
 
-          <div className='pt-6 p-8 bg-white rounded border border-[#DFDEE2]'>
-            <p>Pilih kursi Anda</p>
-            <SelectSeat data={seatesFlight} />
+          <div className='mt-4 bg-white rounded border border-[#DFDEE2] p-4'>
+            <p className='mb-6 text-center'>Pilih kursi</p>
+            <SelectSeat data={seates} handleSelect={handleSelect} />
           </div>
 
-          <button
+          <Button
             type='submit'
-            className='flex items-center justify-center bg-[#4642FF] text-white text-sm rounded font-medium py-4'
+            className='mt-8 flex items-center justify-center bg-[#4642FF] text-white text-sm rounded font-medium py-4 w-52 ml-auto'
           >
-            Simpan
-          </button>
+            Lanjutkan
+          </Button>
+        </form>
+        <div>
+          <div className='bg-white rounded py-4'>
+            <div className='relative pb-6 w-full px-4'>
+              <div className='flex items-center gap-2'>
+                <Image
+                  src={Ic_plane}
+                  alt={`plane destination to ${query?.destination}`}
+                />
+                <p>
+                  {query?.departure} → {query?.destination}
+                </p>
+              </div>
+
+              <div className='mt-2 flex items-center gap-2'>
+                <p className='text-sm text-slate-400'>Berangkat </p>
+                <div className='w-1 h-1 rounded-full bg-gray-300' />
+                <p className='text-sm text-slate-800'>3 Juni 2023</p>
+              </div>
+              <div className='mt-2 flex items-center gap-2'>
+                <p className='text-sm text-slate-800'>Soekarno Hatta </p>
+              </div>
+
+              <div className='w-5 h-5 bg-[#F0F1F6] rounded-full absolute -left-[10px] bottom-0' />
+              <div className='w-5 h-5 bg-[#F0F1F6] rounded-full absolute -right-[10px] bottom-0' />
+              <div className='w-[90%] border border-dashed border-gray-200 absolute bottom-[10px] left-1/2 -translate-x-1/2' />
+            </div>
+            <div className='px-4 pt-4'>
+              <div className='pb-4'>
+                <p className='text-sm text-slate-800'>Rincian harga</p>
+                <div className='mt-2 flex items-center justify-between text-xs'>
+                  <p className='text-slate-400'>Dewasa</p>
+                  <p>
+                    {changeToRupiah(300000).slice(0, -3)}{' '}
+                    <span className='text-gray-300'>x{query?.adult}</span>
+                  </p>
+                </div>
+
+                {'kid' in query ? (
+                  <>
+                    <div className='mt-2 flex items-center justify-between text-xs'>
+                      <p className='text-slate-400'>Anak-anak</p>
+                      <p>
+                        {changeToRupiah(200000).slice(0, -3)}{' '}
+                        <span className='text-gray-300'>x{query?.kid}</span>
+                      </p>
+                    </div>
+                  </>
+                ) : null}
+
+                {'baby' in query ? (
+                  <>
+                    <div className='mt-2 flex items-center justify-between text-xs'>
+                      <p className='text-slate-400'>Bayi</p>
+                      <p>
+                        {changeToRupiah(100000).slice(0, -3)}{' '}
+                        <span className='text-gray-300'>x{query?.baby}</span>
+                      </p>
+                    </div>
+                  </>
+                ) : null}
+              </div>
+
+              <div className='flex justify-between items-center border-t border-gray-200 pt-1'>
+                <p className='text-sm text-slate-400'>Total</p>
+                <p className='font-medium text-slate-800'>
+                  {changeToRupiah(900000).slice(0, -3)}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
-      <div className='bg-white rounded border border-[#DFDEE2] relative p-4'>
-        <p>{query?.departure}</p>
-        <p>{query?.destination}</p>
       </div>
     </div>
   )
