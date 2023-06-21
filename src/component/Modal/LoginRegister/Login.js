e6e1fc97939039772773885e3d38c29e99b8c619
miftahhakim <miftahhakim02@gmail.com>
2023-06-21 11:15:20 +0700

    feat: integrate api

--- src/component/Modal/LoginRegister/Login.js
+++ src/component/Modal/LoginRegister/Login.js
@@ -1,47 +1,53 @@
-import Button from '@/component/Button'
-import Textfield from '@/component/Form/Textfield'
-import TextfieldPassword from '@/component/Form/TextfieldPassword'
-import api from '@/services/api'
-import React, { useState } from 'react'
+import Button from "@/component/Button";
+import Textfield from "@/component/Form/Textfield";
+import TextfieldPassword from "@/component/Form/TextfieldPassword";
+import api from "@/services/api";
+import React, { useState } from "react";
 
 const initialValues = {
-  email: '',
-  password: '',
-}
+  email: "",
+  password: "",
+};
 
 export default function Login({ toggleModal }) {
-  const [form, setForm] = useState(initialValues)
+  const [form, setForm] = useState(initialValues);
 
   async function handleSubmit(e) {
-    e.preventDefault()
-
-    toggleModal()
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://final-project-be-develop.up.railway.app/auth/login",
+        form
+      );
+      console.log(response);
+    } catch (err) {}
+    toggleModal();
   }
 
   function handleChange(e) {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setForm((prev) => ({
       ...prev,
       [name]: value,
-    }))
+    }));
   }
 
   return (
-    <form onSubmit={handleSubmit} className='mt-6'>
-      <div className='flex flex-col gap-6'>
+    <form onSubmit={handleSubmit} className="mt-6">
+      <div className="flex flex-col gap-6">
         <Textfield
-          name='email'
-          id='email'
-          label='email'
+          name="email"
+          id="email"
+          label="email"
           value={form.email}
           onChange={handleChange}
           withLabel
-          placeholder='example@mail.com'
+          placeholder="example@mail.com"
         />
         <TextfieldPassword
-          name='password'
-          id='password'
-          label='Password'
+          name="password"
+          id="password"
+          label="Password"
           value={form.password}
           onChange={handleChange}
         />
@@ -49,11 +55,11 @@ export default function Login({ toggleModal }) {
 
       <Button
         onClick={handleSubmit}
-        type='submit'
-        className='py-4 rounded bg-[#4642FF] text-white font-medium w-full mt-8 hover:shadow hover:shadow-[#4642FF]/50'
+        type="submit"
+        className="py-4 rounded bg-[#4642FF] text-white font-medium w-full mt-8 hover:shadow hover:shadow-[#4642FF]/50"
       >
         Masuk
       </Button>
     </form>
-  )
+  );
 }
