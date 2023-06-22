015295d016db04dd0182e74391ea26ae143e94dc
mulyant20 <mulyan.t20@gmail.com>
2023-06-22 18:49:20 +0700

    feat: success order page

--- src/component/Layout/CheckoutLayout.js
+++ src/component/Layout/CheckoutLayout.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Stepper from '../Stepper'
 import CheckoutNavbar from '../Navbar/CheckoutNavbar'
 
 export default function CheckoutLayout({ index = 1, children }) {
   return (
     <>
-      <CheckoutNavbar index={index} />
-      <main className='pt-[64px] pb-10 max-w-[900px] mx-auto'>{children}</main>
+      {index < 3 ? <CheckoutNavbar index={index} /> : null}
+      {index < 3 ? (
+        <main className='pt-[64px] pb-10 max-w-[900px] mx-auto'>
+          {children}
+        </main>
+      ) : (
+        children
+      )}
     </>
   )
 }
