53149a01d036f8a19a966f53fe30e32289146cda
mulyant20 <mulyan.t20@gmail.com>
2023-07-01 09:09:27 +0700

    style: add padding

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -7,7 +7,7 @@ const MediaQuery = dynamic(() => import('react-responsive'), { ssr: false })
 
 const Footer = () => {
   return (
-    <footer className='bg-white pt-[72px] mt-[120px]'>
+    <footer className='bg-white pt-[72px] mt-[120px] px-4'>
       {/* Media query untuk lebar layar lebih dari atau sama dengan 786px */}
       <MediaQuery minWidth={786}>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-[1200px] mx-auto border-b border-gray-300 pb-14'>
@@ -109,98 +109,96 @@ const Footer = () => {
 
       {/* Media query untuk lebar layar kurang dari 786px */}
       <MediaQuery maxWidth={786}>
-        <div className='px-4'>
-          <div className='grid grid-cols-1 gap-8 pb-12 border-b border-gray-300'>
-            {/* Card 1 */}
-            <div className='flex flex-col'>
-              <h4 className='text-lg font-bold mb-4'>Tripp</h4>
-            </div>
-
-            {/* Card 2 */}
-            <div className='flex flex-col'>
-              <h4 className='font-medium mb-4'>Perusahaan</h4>
-              <ul className='list-none flex flex-col gap-4 text-gray-500'>
-                <li>
-                  <Link href='/'>Blog</Link>
-                </li>
-                <li>
-                  <Link href='/'>Karier</Link>
-                </li>
-                <li>
-                  <Link href='/'>Tentang Kami</Link>
-                </li>
-                <li>
-                  <Link href='/'>Partner</Link>
-                </li>
-              </ul>
-            </div>
-
-            {/* Card 3 */}
-            <div className='flex flex-col'>
-              <h4 className='mb-4 font-medium'>Dukungan</h4>
-              <ul className='list-none flex flex-col gap-4'>
-                <li>
-                  <Link href='/' className='text-gray-500'>
-                    Pusat Bantuan
-                  </Link>
-                </li>
-                <li>
-                  <Link href='/' className='text-gray-500'>
-                    Kebijakan Privasi
-                  </Link>
-                </li>
-              </ul>
-            </div>
-
-            {/* Card 4 */}
-            <div className='flex flex-col'>
-              <h4 className='font-medium mb-4'>Hubung Kami</h4>
-
-              <div className='flex gap-4'>
-                <Link href='/'>
-                  <Image
-                    height={24}
-                    width={24}
-                    src='icon/facebook.svg'
-                    alt='Facebook'
-                    className='w-8 h-8'
-                  />
-                </Link>
-
-                <Link href='/'>
-                  <Image
-                    height={24}
-                    width={24}
-                    src='icon/instagram.svg'
-                    alt='Instagram'
-                    className='w-8 h-8'
-                  />
-                </Link>
-
-                <Link href='/'>
-                  <Image
-                    height={24}
-                    width={24}
-                    src='icon/twitter.svg'
-                    alt='Twitter'
-                    className='w-8 h-8'
-                  />
-                </Link>
-
-                <Link href='/'>
-                  <Image
-                    height={24}
-                    width={24}
-                    src='icon/whatsapp.svg'
-                    alt='Facebook'
-                    className='w-8 h-8'
-                  />
-                </Link>
-              </div>
-            </div>
-
-            {/* Akhir Card 4 */}
+        <div className='grid grid-cols-1 gap-8 pb-12 border-b border-gray-300'>
+          {/* Card 1 */}
+          <div className='flex flex-col'>
+            <h4 className='text-lg font-bold mb-4'>Tripp</h4>
           </div>
+
+          {/* Card 2 */}
+          <div className='flex flex-col'>
+            <h4 className='font-medium mb-4'>Perusahaan</h4>
+            <ul className='list-none flex flex-col gap-4 text-gray-500'>
+              <li>
+                <Link href='/'>Blog</Link>
+              </li>
+              <li>
+                <Link href='/'>Karier</Link>
+              </li>
+              <li>
+                <Link href='/'>Tentang Kami</Link>
+              </li>
+              <li>
+                <Link href='/'>Partner</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Card 3 */}
+          <div className='flex flex-col'>
+            <h4 className='mb-4 font-medium'>Dukungan</h4>
+            <ul className='list-none flex flex-col gap-4'>
+              <li>
+                <Link href='/' className='text-gray-500'>
+                  Pusat Bantuan
+                </Link>
+              </li>
+              <li>
+                <Link href='/' className='text-gray-500'>
+                  Kebijakan Privasi
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Card 4 */}
+          <div className='flex flex-col'>
+            <h4 className='font-medium mb-4'>Hubung Kami</h4>
+
+            <div className='flex gap-4'>
+              <Link href='/'>
+                <Image
+                  height={24}
+                  width={24}
+                  src='icon/facebook.svg'
+                  alt='Facebook'
+                  className='w-8 h-8'
+                />
+              </Link>
+
+              <Link href='/'>
+                <Image
+                  height={24}
+                  width={24}
+                  src='icon/instagram.svg'
+                  alt='Instagram'
+                  className='w-8 h-8'
+                />
+              </Link>
+
+              <Link href='/'>
+                <Image
+                  height={24}
+                  width={24}
+                  src='icon/twitter.svg'
+                  alt='Twitter'
+                  className='w-8 h-8'
+                />
+              </Link>
+
+              <Link href='/'>
+                <Image
+                  height={24}
+                  width={24}
+                  src='icon/whatsapp.svg'
+                  alt='Facebook'
+                  className='w-8 h-8'
+                />
+              </Link>
+            </div>
+          </div>
+
+          {/* Akhir Card 4 */}
         </div>
 
         {/* copyrighted */}
