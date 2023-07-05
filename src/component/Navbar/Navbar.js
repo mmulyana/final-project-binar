0266fc73fc66757dd3163134f2cb4ab10e034e2a
mulyant20 <mulyan.t20@gmail.com>
2023-07-06 05:51:35 +0700

    update

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -208,79 +208,88 @@ export default function Navbar({ isDark = false }) {
               </nav>
             </div>
 
-            {user !== null ? (
-              <div className='flex gap-4 items-center'>
-                <div className='h-8 w-8 rounded relative flex items-center justify-center'>
-                  <Image src={locale === 'en' ? img_EN : img_ID} alt='change language' onClick={() => setOpenLocale(true)} className='cursor-pointer'/>
-                  {!!openLocale && <LocaleModal setIsOpen={setOpenLocale} ref={LocaleRef}/>}
-                </div>
-                <div
-                  className={[
-                    'relative h-8 w-8 flex items-center justify-center rounded-full',
-                    !isDark
-                      ? offset > 0
-                        ? 'bg-gray-100/90'
-                        : 'bg-white/20'
-                      : 'bg-gray-100/90',
-                  ].join(' ')}
-                >
-                  <Button
+            <div className='flex gap-4 items-center'>
+              <div className='h-8 w-8 rounded relative flex items-center justify-center'>
+                <Image
+                  src={locale === 'en' ? img_EN : img_ID}
+                  alt='change language'
+                  onClick={() => setOpenLocale(true)}
+                  className='cursor-pointer'
+                />
+                {!!openLocale && (
+                  <LocaleModal setIsOpen={setOpenLocale} ref={LocaleRef} />
+                )}
+              </div>
+              {user !== null ? (
+                <>
+                  <div
                     className={[
+                      'relative h-8 w-8 flex items-center justify-center rounded-full',
                       !isDark
                         ? offset > 0
-                          ? 'text-slate-800'
-                          : 'text-white'
-                        : 'text-slate-800',
+                          ? 'bg-gray-100/90'
+                          : 'bg-white/20'
+                        : 'bg-gray-100/90',
                     ].join(' ')}
-                    onClick={() => setOpenNotify(!openNotify)}
                   >
-                    <svg
-                      width='20'
-                      height='20'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
+                    <Button
+                      className={[
+                        !isDark
+                          ? offset > 0
+                            ? 'text-slate-800'
+                            : 'text-white'
+                          : 'text-slate-800',
+                      ].join(' ')}
+                      onClick={() => setOpenNotify(!openNotify)}
                     >
-                      <path
-                        d='M9.35419 21C10.0593 21.6224 10.9856 22 12 22C13.0145 22 13.9407 21.6224 14.6458 21M18 8C18 6.4087 17.3679 4.88258 16.2427 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.8826 2.63214 7.75738 3.75736C6.63216 4.88258 6.00002 6.4087 6.00002 8C6.00002 11.0902 5.22049 13.206 4.34968 14.6054C3.61515 15.7859 3.24788 16.3761 3.26134 16.5408C3.27626 16.7231 3.31488 16.7926 3.46179 16.9016C3.59448 17 4.19261 17 5.38887 17H18.6112C19.8074 17 20.4056 17 20.5382 16.9016C20.6852 16.7926 20.7238 16.7231 20.7387 16.5408C20.7522 16.3761 20.3849 15.7859 19.6504 14.6054C18.7795 13.206 18 11.0902 18 8Z'
-                        stroke='currentColor'
-                        strokeWidth='2'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      />
-                    </svg>
-                  </Button>
-                  {!!data && (
-                    <div className='absolute -top-[5px] -right-[5px] h-4 w-4 rounded-full bg-blue-500 text-white flex items-center justify-center text-xs'>
-                      <p>{data.filter((d) => d.is_read === false).length}</p>
-                    </div>
-                  )}
-                  {!!openNotify && (
-                    <NotificationModal data={data} ref={notificationRef} />
-                  )}
-                </div>
-                <div className='relative h-8 w-8'>
-                  <Button onClick={() => setOpenMenu(!openMenu)}>
-                    <Avvvatars value={user.email} />
-                  </Button>
-                  {!!openMenu && <MenuModal ref={menuRef} data={user} />}
-                </div>
-              </div>
-            ) : (
-              <Button
-                onClick={() => setIsOpen(true)}
-                className={[
-                  'px-6 py-3 font-medium rounded shadow-sm text-sm',
-                  !isDark
-                    ? offset > 0
-                      ? 'bg-[#326BF1] text-white'
-                      : 'text-[#1E1E1E] bg-white'
-                    : 'bg-[#326BF1] text-white',
-                ].join(' ')}
-              >
-                {t('navBtn')}
-              </Button>
-            )}
+                      <svg
+                        width='20'
+                        height='20'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          d='M9.35419 21C10.0593 21.6224 10.9856 22 12 22C13.0145 22 13.9407 21.6224 14.6458 21M18 8C18 6.4087 17.3679 4.88258 16.2427 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.8826 2.63214 7.75738 3.75736C6.63216 4.88258 6.00002 6.4087 6.00002 8C6.00002 11.0902 5.22049 13.206 4.34968 14.6054C3.61515 15.7859 3.24788 16.3761 3.26134 16.5408C3.27626 16.7231 3.31488 16.7926 3.46179 16.9016C3.59448 17 4.19261 17 5.38887 17H18.6112C19.8074 17 20.4056 17 20.5382 16.9016C20.6852 16.7926 20.7238 16.7231 20.7387 16.5408C20.7522 16.3761 20.3849 15.7859 19.6504 14.6054C18.7795 13.206 18 11.0902 18 8Z'
+                          stroke='currentColor'
+                          strokeWidth='2'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                        />
+                      </svg>
+                    </Button>
+                    {!!data && (
+                      <div className='absolute -top-[5px] -right-[5px] h-4 w-4 rounded-full bg-blue-500 text-white flex items-center justify-center text-xs'>
+                        <p>{data.filter((d) => d.is_read === false).length}</p>
+                      </div>
+                    )}
+                    {!!openNotify && (
+                      <NotificationModal data={data} ref={notificationRef} />
+                    )}
+                  </div>
+                  <div className='relative h-8 w-8'>
+                    <Button onClick={() => setOpenMenu(!openMenu)}>
+                      <Avvvatars value={user.email} />
+                    </Button>
+                    {!!openMenu && <MenuModal ref={menuRef} data={user} />}
+                  </div>
+                </>
+              ) : (
+                <Button
+                  onClick={() => setIsOpen(true)}
+                  className={[
+                    'px-6 py-3 font-medium rounded shadow-sm text-sm',
+                    !isDark
+                      ? offset > 0
+                        ? 'bg-[#326BF1] text-white'
+                        : 'text-[#1E1E1E] bg-white'
+                      : 'bg-[#326BF1] text-white',
+                  ].join(' ')}
+                >
+                  {t('navBtn')}
+                </Button>
+              )}
+            </div>
           </MediaQuery>
 
           {/* mobile */}
